062321dcff5fc2209a3f3d547b647a05de75e388
MDEV-2007 <uzbekcoders0706@gmail.com>
2023-12-20 10:52:05 +0500

    Helllo

--- book_app/models.py
+++ book_app/models.py
@@ -14,14 +14,23 @@ class Genre(models.Model):
     
     def __str__(self):
         return self.name
+    
+
+class Language(models.Model):
+    name = models.CharField(max_length=100)
 
 class Book(models.Model):
     title = models.CharField(max_length=250)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author')
     published_time =  models.DateField()
     isbn = models.CharField(max_length=13)
     genre = models.ManyToManyField(Genre)
     image = models.ImageField(upload_to='book_image/')
+    price = models.FloatField(max_length=100)
+    # language = models.ForeignKey(Language,on_delete=models.CASCADE, related_name='author')
+    format = models.CharField(max_length=150)
+    series = models.CharField(max_length=250)
+    
 
     def __str__(self):
         return self.title
@@ -41,6 +50,25 @@ class Book(models.Model):
     @property
     def xrange(self):
         return range(5 - int(self.rating))
+
+    def ratings(self):
+
+        def get(rating) -> dict:
+            
+            filtered = self.reviews.filter(rating=rating)
+
+            return {
+                'count': len(filtered),
+                'percent': round(len(filtered)  / (len(self.reviews.all()) / 100), 1)
+            }
+
+        return {
+            '1': get(1),
+            '2': get(2),
+            '3': get(3),
+            '4': get(4),
+            '5': get(5)
+        }
 
 
 class Review(models.Model):
--- static/js/main.js
+++ static/js/main.js
@@ -1,73 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const categoryCarousel = document.getElementById('categoryCarousel');
-    let isDragging = false;
-    let startX;
-    let scrollLeft;
-  
-    categoryCarousel.addEventListener('mousedown', (e) => {
-      isDragging = true;
-      startX = e.pageX - categoryCarousel.offsetLeft;
-      scrollLeft = categoryCarousel.scrollLeft;
-    });
-  
-    categoryCarousel.addEventListener('mouseleave', () => {
-      isDragging = false;
-    });
-  
-    categoryCarousel.addEventListener('mouseup', () => {
-      isDragging = false;
-    });
-  
-    categoryCarousel.addEventListener('mousemove', (e) => {
-      if (!isDragging) return;
-      e.preventDefault();
-      const x = e.pageX - categoryCarousel.offsetLeft;
-      const walk = (x - startX) * 3;
-      categoryCarousel.scrollLeft = scrollLeft - walk;
-    });
-  
-    categoryCarousel.addEventListener('dragstart', (e) => {
-      e.preventDefault();
-    });
-  
-    categoryCarousel.addEventListener('selectstart', (e) => {
-      e.preventDefault();
-    });
-  });
-  document.addEventListener('DOMContentLoaded', function () {
-    const categoryCarousel = document.getElementById('categoryCarousel');
-    let isDragging = false;
-    let startX;
-    let scrollLeft;
-  
-    categoryCarousel.addEventListener('mousedown', (e) => {
-      isDragging = true;
-      startX = e.pageX - categoryCarousel.offsetLeft;
-      scrollLeft = categoryCarousel.scrollLeft;
-    });
-  
-    categoryCarousel.addEventListener('mouseleave', () => {
-      isDragging = false;
-    });
-  
-    categoryCarousel.addEventListener('mouseup', () => {
-      isDragging = false;
-    });
-  
-    categoryCarousel.addEventListener('mousemove', (e) => {
-      if (!isDragging) return;
-      e.preventDefault();
-      const x = e.pageX - categoryCarousel.offsetLeft;
-      const walk = (x - startX) * 3;
-      categoryCarousel.scrollLeft = scrollLeft - walk;
-    });
-  
-    categoryCarousel.addEventListener('dragstart', (e) => {
-      e.preventDefault();
-    });
-  
-    categoryCarousel.addEventListener('selectstart', (e) => {
-      e.preventDefault();
-    });
-  });
-    
+let indicator = document.querySelector('.scroll-indicator .progress')
+let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+
+
+
+window.addEventListener('scroll', scroll);
+
+function scroll() {
+  let scrollTop = document.documentElement.scrollTop;
+  let scrolled = (scrollTop / scrollHeight) * 100
+  indicator.style.width = `${scrolled}%`;
+}
+
+
+
+
